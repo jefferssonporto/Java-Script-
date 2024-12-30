12cb97621c44d8e7b5726d7f5a62089ce4644fc3
Jefferson Porto <133730356+jefferssonporto@users.noreply.github.com>
2024-12-30 08:57:04 -0300

    updt

--- Projetos/Projeto 15.html/script.js
+++ Projetos/Projeto 15.html/script.js
@@ -1,22 +1,22 @@
 /* Função em JS que troca as imagens da pasta */
-function carregar() {
-    var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem')
+function verificar() {
     var data = new Date()
-    var hora = data.getHours()
-    msg.innerHTML = `Agora são ${hora} horas `
-    if (hora >= 0 &&  hora <12) {
-        //BOM DIA!
-        img.src = 'fotomanha.png'
-        document.body.style.background = '#e2cd9f'
-    } else if (hora >=12 && hora <18) {
-        //BOA TARDE!
-        img.src = 'fototarde.png'
-         document.body.style.background = '#b9846f'
-    } else {
-        //BOA NOITE!
-        img.src = 'fotonoite.png'
-         document.body.style.background = '#515154'
-
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.querySelector('div#res')
+    if (fano.value.lenght == 0 || fano.value > ano ) {
+        window.alert('[ERROR] Verifique os dados e tente novamente!')
+    }  else { 
+        var sex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        var gênero = ''
+        if (fsex[0].checked)    {
+            gênero =   'Homem'
+        } else if (fsex[1].checked) {
+            gênero = 'Mulher'
+        }  
+        res.innerHTML = `Detectamos ${gênero} com ${idade} anos.`
+        }
+        
     }
-}
+
