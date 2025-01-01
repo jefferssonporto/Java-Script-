1e39a1f45e256ea00ac6cb0a491b07b72d371d1c
Jefferson Porto <133730356+jefferssonporto@users.noreply.github.com>
2025-01-01 20:27:44 -0300

    updt

--- Projetos/Projeto 18.html/script.js
+++ Projetos/Projeto 18.html/script.js
@@ -1,2 +1,20 @@
-
+function tabuada() {
+    let num = document.getElementById('txtn')
+    let tab = document.getElementById('seltab')
+    if (num.value == 0 ) {
+        window.alert('Por favor digite um número!') // Se o número for 0 ou nada vai aparecer a mensagem de erro.     
+    } else {
+        let n = Number(num.value) 
+        let c = 1
+        tab.innerHTML = '' //Limpa a tabuada para mostrar a próxima, substitui
+        while (c <= 10) {
+            let item = document.createElement('option') //criação a opção do SELECT do HTML.
+                item.text = `${n} x ${c} = ${n*c}` // Calcula a operação
+                item.value = `tab${c}` //Para PHP na ordem ficaria tab1,tab2, tab3, tab4....
+                tab.appendChild(item)
+                c++
+        }
+    }
+    
+}
 
